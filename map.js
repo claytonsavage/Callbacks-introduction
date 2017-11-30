9dec0e7925714eabeee432f080f8b5d6cd1d4f6f
claytonsavage <clytnsavage@gmail.com>
2017-11-30 23:05:12 +0000

    added the map.js

---
+++ map.js
@@ -0,0 +1,12 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+var transform = function( elements, callback ) {
+  var newMap = [];
+  elements.forEach(function(element) {newMap.push(callback(element))} );
+  return newMap;
+}
+
+
+console.log(transform(words, function(word){return word.length}));
+
+
